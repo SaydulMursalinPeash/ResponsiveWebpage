5b5082c7bc0c25355464993edeb9ab186a66a10a
SaydulMursalinPeash <60428+SaydulMursalinPeash@users.noreply.github.com>
2021-06-07 15:44:35 +0600

    with js

---
+++ page.js
@@ -0,0 +1,7 @@
+{
+    let other=document.getElementsByClassName("other");
+    let floating=document.getElementsByClassName("floating-menu");
+    floating.addEventListener("click",function(){
+        other.
+    })
+}
